630288ab803d3935c752d245ab6fa9f563c368af
VIRAJJADHAV0025 <virajjadhav802@gmail.com>
2026-08-29 22:06:38 +0530

    Add more patterns

--- patterns/All_Patterns/PATTERNS.java
+++ patterns/All_Patterns/PATTERNS.java
@@ -71,6 +71,7 @@ public class PATTERNS {
         }
         System.out.println();
     }
+
     public static void pattern5(int n) {
         /*
 
@@ -100,6 +101,165 @@ public class PATTERNS {
         }
         System.out.println();
     }
+
+    public static void pattern7(int n) {
+        /*        
+            *
+           ***
+          *****
+         *******
+        *********
+
+        */
+        for(int i=0; i<n; i++) {
+            //space
+            for(int j=0; j<n-i-1; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=0; j<(2*i)+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void pattern8(int n){
+        /*
+        *********
+         *******
+          *****
+           ***
+            *
+        */
+        for(int i=0; i<n; i++) {
+            //space
+            for(int j=0; j<i; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=0; j<2*n-(2*i+1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void pattern9(int n) {
+        /* 
+        Combined pattern 7 & 8
+
+            *
+           ***
+          *****
+         *******
+        *********
+        *********
+         *******
+          *****
+           ***
+            *
+        */ 
+
+        for(int i=0; i<n; i++) {
+            //space
+            for(int j=0; j<n-i-1; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=0; j<(2*i)+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=0; i<n; i++) {
+            //space
+            for(int j=0; j<i; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=0; j<2*n-(2*i+1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void pattern10(int n) {
+        /*
+
+        *
+        **
+        ***
+        ****
+        *****
+        ****
+        ***
+        **
+        *
+
+        */
+        for(int i=1; i<=2*n-1; i++) {
+            int stars = i;
+            if(i > n)
+                stars = 2*n - i;
+            for(int j=1; j<=stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void pattern11(int n) {
+        /*
+        1 
+        0 1 
+        1 0 1 
+        0 1 0 1 
+        1 0 1 0 1
+        */
+        int start = 1;
+        for(int i=0; i<n; i++) {
+            if(i % 2 == 0)
+                start = 1;
+            else
+                start = 0;
+
+            for(int j=0; j<=i; j++) {
+                System.out.print(start + " ");
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void pattern12(int n) {
+        int space = 2 * (n-1);
+        for(int i=1; i<=n; i++) {
+            //numbers
+            for(int j=1; j<=i; j++) {
+                System.out.print(j);
+            }
+            //spaces
+            for(int j=1; j<=space; j++) {
+                System.out.print(" ");
+            }
+            //numbers
+            for(int j=i; j>=1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+            space -= 2;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
@@ -112,5 +272,13 @@ public class PATTERNS {
         pattern4(n);
         pattern5(n);
         pattern6(n);
+        pattern7(n);
+        pattern8(n);
+        pattern9(n);
+        pattern10(n);
+        pattern11(n);
+        pattern12(n);
+
+        sc.close();
     }
 }
